8f4be70712fe6bd6df610be6bab0f417a142da07
NYaCodes <107141629+NYaCodes@users.noreply.github.com>
2023-01-22 19:02:10 -0600

    Updating apiUrl & app.js

--- src/app.js
+++
@@ -1,9 +0,0 @@
-let apiKey = "7a02a4ad0d5tebob4bd0bb4b9af39320";
-let apiUrl =  `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-
-
-function displayTemperature(response) {
-  console.log(response.data);
-}
-
- axios.get(apiUrl).then(displayTemperature);`;
